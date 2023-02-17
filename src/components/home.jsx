fcbc787dfe49fe9a805ca8c5c6165d00699492b6
PierreNV <pierre_n@live.fr>
2023-02-17 12:33:10 +0100

    commit intro txt

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 const Home = () => {
-	return <h1 className="h3 mb-5 fw-normal">Welcome to Movly</h1>;
+	return <div className="mb-2">
+		<h1 className="h3 mb-5 fw-normal">Welcome to Movly</h1>
+		<p>Movly is a pure fictive react application intended to demonstrate some of my react JS knowledge aquired along my training.</p>
+		<p>First, you want to create a user account that grants you access to a few extra functionalities such as adding movies to the database in the movies page. Some of these functionalities are admin only and might still appear disabled to you.</p>
+		<p></p>
+		<p>API express</p>
+		<p>As you probably noticed, I have used bootstrap for the style. It is minimalist and mobile first, it gives a consistent experience to the user on various medias.</p>
+
+	</div>;
 };
 
 export default Home;

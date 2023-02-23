994276be0c89a5f3249d1b66d116e52fa1b32758
PierreNV <pierre_n@live.fr>
2023-02-23 16:02:42 +0100

    edit

--- src/services/servHTTP.js
+++ src/services/servHTTP.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
- axios.interceptors.response.use(null, (error) => {
+axios.interceptors.response.use(null, (error) => {
 	const expectedError =
 	error.response && error.response.status >= 400 && error.response.status < 500;
 	if (!expectedError) {

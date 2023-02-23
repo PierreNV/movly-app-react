0bdc71664dc208a36313a45afe7fa3480d891e8c
PierreNV <pierre_n@live.fr>
2023-02-23 16:02:11 +0100

    simple alert message

--- src/services/servHTTP.js
+++ src/services/servHTTP.js
@@ -1,18 +1,15 @@
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-// import { toast } from "react-toastify";
-// import servlogger from "./servlogger";
 
-// axios.interceptors.response.use(null, (error) => {
-// 	const expectedError =
-// 		error.response && error.response.status >= 400 && error.response.status < 500;
-// 	if (!expectedError) {
-// 		servlogger.log("Error log:", error);
-// 		toast.error("Unexpected error");
-// 	}
-// 	return Promise.reject(error);
-// });
+ axios.interceptors.response.use(null, (error) => {
+	const expectedError =
+	error.response && error.response.status >= 400 && error.response.status < 500;
+	if (!expectedError) {
+	alert(error.message);
+	}
+	return Promise.reject(error);
+});
 
 export function setJWT(jwt) {
 	axios.defaults.headers.common["x-auth-token"] = jwt;

5dcbe08fd17106e0d4e0e22050a0b57147dfc67a
PierreNV <pierre_n@live.fr>
2024-04-21 14:28:38 +0200

    modules conv

--- src/components/common/withRouter.jsx
+++ src/components/common/withRouter.jsx
@@ -1,13 +1,7 @@
 import React from "react";
+
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 
 export function withRouter(Component) {
-	return (props) => (
-		<Component
-			{...props}
-			params={useParams()}
-			navigate={useNavigate()}
-			location={useLocation()}
-		/>
-	);
+  return (props) => <Component {...props} params={useParams()} navigate={useNavigate()} location={useLocation()} />;
 }
--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { logout } from "../services/servAuth";
+import { logout } from "../services/servAuth.mjs";
 import { withRouter } from "./common/withRouter";
 
 const Logout = (props) => {
-	const onSignOut = async () => {
-		logout();
-		props.navigate("/");
-		props.navigate(0);
-	};
-	return (
-		<NavLink
-			type="button"
-			className="nav-link"
-			aria-current="page"
-			onClick={() => onSignOut()}>
-			Sign out
-		</NavLink>
-	);
+  const onSignOut = async () => {
+    logout();
+    props.navigate("/");
+    props.navigate(0);
+  };
+  return (
+    <NavLink type="button" className="nav-link" aria-current="page" onClick={() => onSignOut()}>
+      Sign out
+    </NavLink>
+  );
 };
 
 export default withRouter(Logout);

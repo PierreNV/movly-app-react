a049151d490164130a56f54e074a358fa4ce4bf5
PierreNV <pierre_n@live.fr>
2024-04-21 14:30:35 +0200

    modules conv

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { getCurrentUser } from "./services/servAuth";
+import { getCurrentUser } from "./services/servAuth.mjs";
 import Movies from "./components/movies";
 import Home from "./components/home";
 import Customers from "./components/customers";
@@ -33,35 +33,15 @@ class App extends Component {
         <main className="container-xxl my-md-4 bd-layout">
           <div className="m-3">
             <Routes>
-              <Route
-                path="*"
-                element={<Navigate to="/not-found" replace={true} />}
-              />
+              <Route path="*" element={<Navigate to="/not-found" replace={true} />} />
               <Route path="/" element={<Home />} />
-              <Route
-                path="/login"
-                element={<LoginForm user={this.state.user} />}
-              />
+              <Route path="/login" element={<LoginForm user={this.state.user} />} />
               <Route path="/register" element={<RegisterForm />} />
-              <Route
-                path="/movies/*"
-                element={
-                  <Movies user={this.state.user} isAdmin={this.state.isAdmin} />
-                }
-              />
-              <Route
-                path="/movies/:id"
-                element={<UserLock component={MovieForm} />}
-              />
-              <Route
-                path="/customers"
-                element={<AdminLock component={Customers} />}
-              />
+              <Route path="/movies/*" element={<Movies user={this.state.user} isAdmin={this.state.isAdmin} />} />
+              <Route path="/movies/:id" element={<UserLock component={MovieForm} />} />
+              <Route path="/customers" element={<AdminLock component={Customers} />} />
               <Route path="/rentals" element={<Rentals />} />
-              <Route
-                path="/profile"
-                element={<Profile user={this.state.user} />}
-              />
+              <Route path="/profile" element={<Profile user={this.state.user} />} />
               <Route path="/not-found" element={<NotFound />} />
             </Routes>
           </div>

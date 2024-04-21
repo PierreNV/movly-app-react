ac567dbc22d140eb40e62b4e822fa30bdb87f959
PierreNV <pierre_n@live.fr>
2024-04-21 14:04:00 +0200

    api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,14 +1,21 @@
 import express from "express";
 import serverless from "serverless-http";
 import cors from "cors";
+import db from "./startup/db";
+import validation from "./startup/validation";
+import routes from "./startup/routes";
 
 const api = express();
-api.use(cors());
 
-require("./startup/db").default();
-require("./startup/config").default();
-require("./startup/validation").default();
-require("./startup/routes").default(api);
+api.use(cors());
+api.use(db());
+api.use(validation());
+api.use(routes(api));
+
+// require("./startup/db").default();
+// require("./startup/config").default();
+// require("./startup/validation").default();
+// require("./startup/routes").default(api);
 
 // const server = serverless(api);
 

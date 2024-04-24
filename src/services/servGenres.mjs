e1814f0c4476e6e7b872bc61410cdd482b7d214b
PierreNV <pierre_n@live.fr>
2024-04-24 22:45:08 +0200

    async

--- src/services/servGenres.mjs
+++ src/services/servGenres.mjs
@@ -1,6 +1,6 @@
 // import servHTTP from "./servHTTP.mjs";
 // const endPoint = "/genres";
 
-export function getGenres() {
-  return fetch("./netlify/functions/genres");
+export async function getGenres() {
+  return await fetch("./netlify/functions/genres");
 }

31a7f9c758fbf1254d904a30aeadc5db882b9bed
PierreNV <pierre_n@live.fr>
2024-06-21 15:23:05 +0200

    sort icon fix

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -1,56 +1,37 @@
 import React, { Component } from "react";
+import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
 
 class TableHeader extends Component {
-	sortColumn = (path) => {
-		const sortedColumn = { ...this.props.sortedColumn };
-		if (sortedColumn.path === path)
-			sortedColumn.order = sortedColumn.order === "asc" ? "desc" : "asc";
-		else {
-			sortedColumn.path = path;
-			sortedColumn.order = "asc";
-		}
-		this.props.onSort(sortedColumn);
-	};
-	renderSortIcon = (column) => {
-		const { sortedColumn } = this.props;
-		if (column.key) return null;
-		if (column.path !== sortedColumn.path)
-			return (
-				<i
-					className="fa fa-sort "
-					aria-hidden="true"
-					role="button"></i>
-			);
-		if (sortedColumn.order === "asc")
-			return (
-				<i
-					className="fa fa-sort-asc"
-					aria-hidden="true"
-					role="button"></i>
-			);
-		return (
-			<i
-				className="fa fa-sort-desc"
-				aria-hidden="true"
-				role="button"></i>
-		);
-	};
-	render() {
-		return (
-			<thead>
-				<tr>
-					{this.props.columns.map((column) => (
-						<th
-							key={column.path || column.key}
-							onClick={() => this.sortColumn(column.path)}>
-							<div>{column.label}</div>
-							<div>{this.renderSortIcon(column)}</div>
-						</th>
-					))}
-				</tr>
-			</thead>
-		);
-	}
+  sortColumn = (path) => {
+    const sortedColumn = { ...this.props.sortedColumn };
+    if (sortedColumn.path === path) sortedColumn.order = sortedColumn.order === "asc" ? "desc" : "asc";
+    else {
+      sortedColumn.path = path;
+      sortedColumn.order = "asc";
+    }
+    this.props.onSort(sortedColumn);
+  };
+  renderSortIcon = (column) => {
+    const { sortedColumn } = this.props;
+    if (column.key) return null;
+    if (column.path !== sortedColumn.path) return <FaSort />;
+    if (sortedColumn.order === "asc") return <FaSortDown />;
+    return <FaSortUp />;
+  };
+  render() {
+    return (
+      <thead>
+        <tr>
+          {this.props.columns.map((column) => (
+            <th key={column.path || column.key} onClick={() => this.sortColumn(column.path)}>
+              <div>{column.label}</div>
+              <div>{this.renderSortIcon(column)}</div>
+            </th>
+          ))}
+        </tr>
+      </thead>
+    );
+  }
 }
 
 export default TableHeader;

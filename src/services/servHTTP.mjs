6f6af7fb6100c14abb0c7b144c1b9c4c7d34c350
PierreNV <pierre_n@live.fr>
2024-09-14 11:18:51 +0200

    staged only param key

--- src/services/servHTTP.mjs
+++ src/services/servHTTP.mjs
@@ -1,17 +1,8 @@
 import axios from "axios";
-import { servLogger } from "./servLog.mjs";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.params = { secret: `mysecret` };
 axios.defaults.headers.common = { "Content-Type": "application/json", Accept: "application/json" };
-axios.defaults.params = { secret: `${process.env.REACT_APP_SECRET}` };
-
-axios.interceptors.response.use(null, (error) => {
-  const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
-  if (!expectedError) {
-    servLogger(error);
-  }
-  return Promise.reject(error);
-});
 
 const axiosMethods = {
   get: axios.get,
